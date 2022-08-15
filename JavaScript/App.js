689d7d06428b8e9ee9a241ba9da4153ff6a21c1f
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-15 23:09:47 +0500

    Nav-bar Created

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,7 +1,7 @@
 const getElement=(selector)=>{
     const element=document.querySelector(selector);
     if(element)
-      {
+      {   
         return element;
       }
       throw Error(`Please double check your class name, there is no${selector} class`);
@@ -9,7 +9,7 @@ const getElement=(selector)=>{
 
 const links=getElement('.nav-links');
 const navBtnDOM=getElement('.nav-btn');
-navBtnDOM.addEventListener('click',()=>{links.classList.toggle('show-links')});
+navBtnDOM.addEventListener('click',()=>{links.classList.toggle('show-link')});
 
 const date=getElement('#date');
 const currentYear=new Date().getFullYear();
