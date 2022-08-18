1ca3cc7235bef811100dae3b710742f16b9f0ff2
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-18 23:18:20 +0500

    Home page is completed

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -7,10 +7,13 @@ const getElement=(selector)=>{
       throw Error(`Please double check your class name, there is no${selector} class`);
 };
 
+
 const links=getElement('.nav-links');
 const navBtnDOM=getElement('.nav-btn');
+//Controlling the hide and visible of the menu
 navBtnDOM.addEventListener('click',()=>{links.classList.toggle('show-link')});
 
+//Setting the Date on the nav bar
 const date=getElement('#date');
 const currentYear=new Date().getFullYear();
 console.log(currentYear);
